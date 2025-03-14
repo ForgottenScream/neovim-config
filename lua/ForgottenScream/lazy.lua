c639633dd7fc1717d32eaa8154f47fa755820f74
ForgottenScream <150345575+ForgottenScream@users.noreply.github.com>
2025-03-14 18:03:37 +0000

    getting lsp sorted, finally.

--- lua/ForgottenScream/lazy.lua
+++ lua/ForgottenScream/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("ForgottenScream.plugins", {
+  require("lazy").setup({ { import = "ForgottenScream.plugins" }, { import = "ForgottenScream.plugins.lsp" } }, {
   checker = {
     enabled = true,
     notify = false,

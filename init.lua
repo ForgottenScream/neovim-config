d8c100aed556d1102024719b44fc00181909308f
ForgottenScream <150345575+ForgottenScream@users.noreply.github.com>
2025-03-14 16:20:12 +0000

    issues with leader key and lazy but its working rn so backup time

--- init.lua
+++ init.lua
@@ -1,1 +1,2 @@
 require("ForgottenScream.core")
+require("ForgottenScream.lazy")
--- lua/ForgottenScream/core/keymaps.lua
+++ lua/ForgottenScream/core/keymaps.lua
@@ -3,3 +3,4 @@ vim.g.mapleader = " "
 local keymap = vim.keymap -- for readability
 
 keymap.set("i", "ne", "<ESC>", {desc = "Exit insert mode with ne"})
+keymap.set("n", "<leader>pv", vim.cmd.Ex)

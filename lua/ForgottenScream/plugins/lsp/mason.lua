e067fd05ad9bada940dd3e2e45e66d2e0f46ab23
ForgottenScream <150345575+ForgottenScream@users.noreply.github.com>
2025-03-15 04:36:28 +0000

    config for stylua

--- lua/ForgottenScream/plugins/lsp/mason.lua
+++ lua/ForgottenScream/plugins/lsp/mason.lua
@@ -2,6 +2,7 @@ return {
   "williamboman/mason.nvim",
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
   },
   config = function()
     -- import mason
@@ -9,6 +10,8 @@ return {
 
     -- import mason-lspconfig
     local mason_lspconfig = require("mason-lspconfig")
+
+    local mason_tool_installer = require("mason-tool-installer")
 
     -- enable mason and configure icons
     mason.setup({
@@ -24,8 +27,6 @@ return {
     mason_lspconfig.setup({
       -- list of servers for mason to install
       ensure_installed = {
-        "java_language_server",
-        "hls",
         "html",
         "cssls",
         "tailwindcss",
@@ -35,6 +36,17 @@ return {
         "emmet_ls",
         "prismals",
         "pyright",
+      },
+    })
+
+    mason_tool_installer.setup({
+      ensure_installed = {
+        "prettier", -- prettier formatter
+        "stylua", -- lua formatter
+        "isort", -- python formatter
+        "black", -- python formatter
+        "hlint", -- haskell linter
+        "checkstyle", -- java linter
       },
     })
   end,

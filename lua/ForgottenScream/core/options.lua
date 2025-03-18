25f6e270ace3941e01a38f338e07a4f5ce9f0f7d
ForgottenScream <150345575+ForgottenScream@users.noreply.github.com>
2025-03-18 14:40:09 +0000

    making java work

--- lua/ForgottenScream/core/options.lua
+++ lua/ForgottenScream/core/options.lua
@@ -1,21 +1,30 @@
-vim.cmd("let g:netrw_liststyle = 3") -- not working for whatever reason, should make it look like a tree
+vim.cmd("let g:netrw_liststyle = 3") -- Not working for whatever reason, should make it look like a tree
 
 local opt = vim.opt -- for readability
 
-opt.relativenumber = true -- shows line number relative to where the cursor is
-opt.number = true -- shows absolute line number for the buffer
+opt.relativenumber = true -- Shows line number relative to where the cursor is
+opt.number = true -- Shows absolute line number for the buffer
 
--- tabs & indentation (getting this out of the way)
-opt.tabstop = 2 -- 2 spaces for tabs
-opt.shiftwidth = 2 -- 2 spaces for ident width
+-- Tabs & indentation (getting this out of the way)
+opt.tabstop = 4 -- 4 spaces for tabs
+opt.shiftwidth = 4 -- 4 spaces for indent width
 opt.expandtab = true --expand tab to spaces
-opt.autoindent = true -- copy indent from current line when starting new one
+opt.autoindent = true -- Copy indent from current line when starting new one
 
 opt.wrap = false --disables line wrapping
 
+-- Java specific settings
+vim.api.nvim_create_autocmd("FileType", {
+	pattern = "java",
+	callback = function()
+		vim.opt_local.tabstop = 4
+		vim.opt_local.shiftwidth = 4
+		vim.opt_local.expandtab = true
+	end,
+})
 --search settings
 opt.ignorecase = true -- ignore case when searching
-opt.smartcase = true -- assumes you want case-sensitive if mixxed case in search
+opt.smartcase = true -- Assumes you want case-sensitive if mixed case in search
 
 -- cursor line
 
@@ -23,19 +32,19 @@ opt.cursorline = true
 
 -- appearance
 
-opt.termguicolors = true --useful for colorschemes to work
-opt.background = "dark" --if there is an option, it will be dark.
-opt.signcolumn = "yes" --show sign column so that text doesn't shift
+opt.termguicolors = true --Useful for color schemes to work
+opt.background = "dark" --If there is an option, it will be dark.
+opt.signcolumn = "yes" --Show sign column so that text doesn't shift
 
 -- backspace
-opt.backspace = "indent,eol,start" -- allows backspace on indent, end of line or insert mode start position
+opt.backspace = "indent,eol,start" -- Allows backspace on indent, end of line or insert mode start position
 
 -- clipboard
-opt.clipboard:append("unnamedplus") -- use system clipboard as default register
+opt.clipboard:append("unnamedplus") -- Use system clipboard as default register
 
 -- split windows
-opt.splitright = true -- split vertical window to the right
-opt.splitbelow = true -- split horizontal window to the bottom
+opt.splitright = true -- Split vertical window to the right
+opt.splitbelow = true -- Split horizontal window to the bottom
 
 -- turn off swapfile
 opt.swapfile = false

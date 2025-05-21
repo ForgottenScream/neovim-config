1a221d47e1ff854d5342a0d82f65a26ce5e5731d
ForgottenScream <150345575+ForgottenScream@users.noreply.github.com>
2025-05-21 02:08:24 +0100

    it worked now, nice

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,30 +1,44 @@
 local lspconfig = require("lspconfig")
 
--- Diagnostics signs (optional visual tweak)
-local signs = { Error = "✘", Warn = "▲", Hint = "⚑", Info = "" }
+-- Custom diagnostic icons
+local signs = {
+  Error = "X",
+  Warn  = "!",
+  Hint  = "?",
+  Info  = "i",
+}
+
 for type, icon in pairs(signs) do
   local hl = "DiagnosticSign" .. type
   vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
--- Basic LSP keymaps
+-- Buffer-local LSP keybindings
 vim.api.nvim_create_autocmd("LspAttach", {
-  callback = function(event)
-    local buf = event.buf
-    local map = function(mode, lhs, rhs)
-      vim.keymap.set(mode, lhs, rhs, { buffer = buf })
+  callback = function(args)
+    local map = function(mode, lhs, rhs, desc)
+      vim.keymap.set(mode, lhs, rhs, {
+        buffer = args.buf,
+        desc = desc,
+        silent = true,
+        noremap = true,
+      })
     end
 
-    map("n", "gd", vim.lsp.buf.definition)
-    map("n", "K", vim.lsp.buf.hover)
-    map("n", "gi", vim.lsp.buf.implementation)
-    map("n", "<leader>rn", vim.lsp.buf.rename)
-    map("n", "<leader>ca", vim.lsp.buf.code_action)
-    map("n", "gr", vim.lsp.buf.references)
-    map("n", "<leader>f", function() vim.lsp.buf.format { async = true } end)
+    -- Navigation and info
+    map("n", "gd", vim.lsp.buf.definition,       "Go to Definition")
+    map("n", "gD", vim.lsp.buf.declaration,      "Go to Declaration")
+    map("n", "gi", vim.lsp.buf.implementation,   "Go to Implementation")
+    map("n", "gr", vim.lsp.buf.references,       "Find References")
+    map("n", "K",  vim.lsp.buf.hover,            "Hover Documentation")
+
+    -- Code actions and refactors
+    map("n", "<leader>rn", vim.lsp.buf.rename,   "Rename Symbol")
+    map("n", "<leader>ca", vim.lsp.buf.code_action, "Code Action")
+
+    -- Formatting
+    map("n", "<leader>f", function()
+      vim.lsp.buf.format({ async = true })
+    end, "Format Buffer")
   end,
 })
-
--- Load each language server
-require("lsp.servers.lua_ls")
--- You can add: require("lsp.servers.pyright"), etc.

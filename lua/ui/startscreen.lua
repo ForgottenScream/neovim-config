ad668592161585c9e2f12dbabad7a444b3f48de7
ForgottenScream <150345575+ForgottenScream@users.noreply.github.com>
2025-05-21 15:45:53 +0100

    making it look nicer

--- lua/ui/startscreen.lua
+++ lua/ui/startscreen.lua
@@ -1,36 +1,52 @@
 local banner = {
-      "                                                     ",
-      "  ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗ ",
-      "  ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║ ",
-      "  ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║ ",
-      "  ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║ ",
-      "  ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║ ",
-      "  ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝ ",
-	  "                                                     ",
-	  "                                                     ",
-	  "                                                     ",
-	  "                                                     ",
-	  "                   n > New File                      ",
-	  "                                                     ",
-	  "                   r > Open Recent File              ",
-	  "                                                     ",
-	  "                   q > Quit                          ",
-      "                                                     ",
-    }
+	"                                                     																															",
+	"                                                     																															",
+	"                                                     																															",
+	"                                                     																															",
+	"                                                      																															",
+	"                                                      																															",
+	"                                                                                             																					",
+	"                                                                                             																					",
+	"                                                                                             																					",
+	"                                                                                             																					",
+	"                                                                                             																					",
+	"                                                                                             																					",
+	"                                                                                             																					",
+	"                                                                                             																					",
+	"                                                                                             																					",
+	"                                                                                             																					",
+	"                                                                                             																					",
+	"                                                      		███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗                                                     				",
+	"                                                      		████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║                                                     				",
+	"                                                      		██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║                                                     				",
+	"                                                      		██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║                                                     				",
+	"                                                      		██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║                                                     				",
+	"                                                      		╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝                                                     				",
+	"                                                                                                                                                             					",
+	"                                                     																															",
+	"                                                     																															",
+	"                                                     																															",
+	"                   														n > New File                      																	",
+	"                                                     																															",
+	"                   														r > Open Recent File              																	",
+	"                                                     																															",
+	"                   														q > Quit                          																	",
+	"                                                     																															",
+}
 
 vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    if vim.fn.argc() == 0 then
-      vim.cmd("enew")
-      vim.api.nvim_buf_set_lines(0, 0, -1, false, banner)
-      vim.bo.modified = false
-      vim.bo.buflisted = false
-      vim.bo.bufhidden = "wipe"
-      vim.bo.filetype = "startscreen"
+	callback = function()
+		if vim.fn.argc() == 0 then
+			vim.cmd("enew")
+			vim.api.nvim_buf_set_lines(0, 0, -1, false, banner)
+			vim.bo.modified = false
+			vim.bo.buflisted = false
+			vim.bo.bufhidden = "wipe"
+			vim.bo.filetype = "startscreen"
 
-      vim.keymap.set("n", "n", ":enew<CR>", { buffer = true })
-      vim.keymap.set("n", "q", ":qa<CR>", { buffer = true })
-	  vim.keymap.set("n", "r", ":browse oldfiles<CR>", { buffer = true})
-    end
-  end,
+			vim.keymap.set("n", "n", ":enew<CR>", { buffer = true })
+			vim.keymap.set("n", "q", ":qa<CR>", { buffer = true })
+			vim.keymap.set("n", "r", ":browse oldfiles<CR>", { buffer = true })
+		end
+	end,
 })
